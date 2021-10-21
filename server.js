2d056192e93051339c01030c35748c3f5841cbe8
unknown <suprakron15@gmail.com>
2021-10-22 02:42:43 +0700

    edit result index

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const fs = require('fs');
-//let cors = require("cors");
+// let cors = require("cors");
 var bodyParser = require('body-parser');
 var path = require('path');
 mongoose.connect("mongodb://localhost/Temp").then(() => {console.log("db connectting")}).catch((error) => {throw error})  //ใส่เพื่อให้มันรันได้ผ่าน port 
-const temp = require("./models/temps") 
+const temp = require("./models/temp") 
 const app = express();
-//app.use(cors());
+// app.use(cors());
 
 
 
 app.get('/', async (req,res) =>{
     
-    res.sendFile(path.join(__dirname + '/public/index2.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.get('/temp', async (req, res) => {
